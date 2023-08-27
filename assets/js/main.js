d1abcca149581fa40c2ebb706a1e32754a12e61b
Yan Bernardo <ybndo.profi@hotmail.com>
2023-08-27 19:33:50 -0300

    Funcionalidade do site terminada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,30 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
-const maxRecords = 151
-const limit = 10;
+const maxRecords = 151;
+const limit = 15;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}" id="num${pokemon.number}" onclick="showPokemonDetails(${pokemon.number})">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
+            ${insertPokemonSmallLiHtml(pokemon)}
         </li>
+    `
+}
+
+function insertPokemonSmallLiHtml(pokemon) {
+    return `
+    <span class="number">#${pokemon.number}</span>
+    <span class="name">${pokemon.name}</span>
+
+    <div class="detail">
+        <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+        </ol>
+
+        <img src="${pokemon.photo}"
+             alt="${pokemon.name}">
+    </div>
     `
 }
 
@@ -45,13 +51,13 @@ function convertDetailedPokemonToLi(pokemon) {
             </ol>
             <ul class="infoUl">
                 <li>
-                    <span class="category">Altura:</span> <span class="category-value">${pokemon.height/10} m</span>
+                    <span class="category ${pokemon.type}">Altura:</span> <span class="category-value ${pokemon.type}">${pokemon.height/10} m</span>
                 </li>
                 <li>
-                    <span class="category">Peso:</span> <span class="category-value">${pokemon.weight/10} kg</span>
+                    <span class="category ${pokemon.type}">Peso:</span> <span class="category-value ${pokemon.type}">${pokemon.weight/10} kg</span>
                 </li>
                 <li>
-                    <span class="category">Habilidades:</span> <span class="category-value">${pokemon.abilities}</span>
+                    <span class="category ${pokemon.type}">Habilidades:</span> <span class="category-value"><span class="">${pokemon.abilities.map((ability) => `${ability}`).join(', ')}</span></span>
                 </li>
                 <div class="clear"></div>
             </ul>
@@ -116,20 +122,20 @@ function showPokemonDetails(pokemonId) {
     //Se encontrar a classe "openedPokemon" ele retira a classe (fecha)
     if ( indexCasoAberto != -1) { 
         liPokeClassAttributes.pop(indexCasoAberto)
-        liPokemon.setAttribute("class", liPokeClassAttributes.join(' '))
 
         let pokemon = pokeApi.getPokemon(pokemonId)
-            .then(convertPokemonToLi)
+            .then(insertPokemonSmallLiHtml)
             .then((stringPokemon) => {
+                liPokemon.setAttribute("class", liPokeClassAttributes.join(' '))
                 liPokemon.innerHTML = stringPokemon
             })
     //Se NÃO encontrar a classe "openedPokemon" ele insere a classe (abre)
     } else { 
-        liPokemon.getAttribute("class")
-        liPokemon.setAttribute("class", liPokemon.getAttribute("class") + " openedPokemon")
+
         let pokemon = pokeApi.getPokemon(pokemonId).then((pokemon) => {
             let newHtml = convertDetailedPokemonToLi(pokemon)
             liPokemon.innerHTML = newHtml;
+            liPokemon.setAttribute("class", liPokemon.getAttribute("class") + " openedPokemon")
         })
         
     }
